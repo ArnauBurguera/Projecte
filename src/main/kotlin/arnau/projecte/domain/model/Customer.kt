f105b37a677dfc26d715601558470afb8e4c0fa9
Arnau Burguera Calles <arnau.burguera@glovoapp.com>
2024-05-26 17:12:35 +0200

    added docker compose and dockerfile and added enum

--- src/main/kotlin/arnau/projecte/domain/model/Customer.kt
+++ src/main/kotlin/arnau/projecte/domain/model/Customer.kt
@@ -1,6 +1,8 @@
 package arnau.projecte.domain.model
 
 import jakarta.persistence.Entity
+import jakarta.persistence.EnumType
+import jakarta.persistence.Enumerated
 import jakarta.persistence.Id
 import jakarta.persistence.Table
 import java.util.UUID
@@ -11,7 +13,14 @@ data class Customer(
         @Id val id: UUID,
         val name: String,
         val bankAccount: String,
+        @Enumerated(EnumType.STRING)
+        val role: CustomerRole
 ) {
-    constructor() : this(UUID.randomUUID(), "", ""){
-    }
+    constructor() : this(UUID.randomUUID(), "", "", CustomerRole.CUSTOM)
+}
+
+enum class CustomerRole {
+    CUSTOM,
+    ADMIN,
+    MODERATOR
 }
