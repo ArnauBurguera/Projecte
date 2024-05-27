be0ec0134684cb83ecd012f76912ef61ab882e41
Arnau Burguera Calles <arnau.burguera@glovoapp.com>
2024-05-27 09:13:48 +0200

    added postgre dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,6 +24,7 @@ dependencies {
     implementation("org.springframework.kafka:spring-kafka")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation ("org.postgresql:postgresql:42.2.18")
     runtimeOnly("com.mysql:mysql-connector-j")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.kafka:spring-kafka-test")
