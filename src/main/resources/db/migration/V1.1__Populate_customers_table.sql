c7225ed0c99767029119c6192c29b1c0b8e6d847
Arnau Burguera Calles <arnau.burguera@glovoapp.com>
2024-06-17 09:09:44 +0200

    Update V2__Populate_customers_table.sql

--- src/main/resources/db/migration/V1.1__Populate_customers_table.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO customers (id, name, bank_account, role) VALUES
-     ('a0e2b0e1-2f89-4c30-b668-c7a1a194a7b1', 'John Doe', '123456789', 'CUSTOM'),
-     ('b1f2c1f2-3fa0-5d41-c779-d8b2b294b8b2', 'Jane Smith', '987654321', 'ADMIN'),
-     ('a7a8a7a8-9ab6-bc07-cfdf-bae8e850e8a8', 'Grace Green', '666777888', 'CUSTOM');
