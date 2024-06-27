5bc853746035fa2bf6764fb9c419f217b6f77133
Arnau Burguera Calles <arnau.burguera@glovoapp.com>
2024-06-27 14:06:22 +0200

    Added intTest config and class

---
+++ src/test/kotlin/arnau/projecte/inTest/repository/CustomerRepositoryIntTest.kt
@@ -0,0 +1,10 @@
+package arnau.projecte.inTest.repository
+
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+@ActiveProfiles("intTest") // Activate the "intTest" profile
+class CustomerRepositoryIntTest {
+    // Your test cases go here
+}
