028bd233e69c74db785d07f57dd62f05d4c54016
Arnau Burguera Calles <arnau.burguera@glovoapp.com>
2024-09-16 09:10:51 +0200

    added some comments

--- src/main/kotlin/arnau/projecte/ProjecteApplication.kt
+++ src/main/kotlin/arnau/projecte/ProjecteApplication.kt
@@ -3,7 +3,7 @@ package arnau.projecte
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
+@SpringBootApplication//(scanBasePackages = ["arnau.projecte.config"])
 class ProjecteApplication
 
 fun main(args: Array<String>) {
--- src/main/kotlin/arnau/projecte/config/SecurityConfig.kt
+++ src/main/kotlin/arnau/projecte/config/SecurityConfig.kt
@@ -5,9 +5,14 @@ import org.springframework.security.config.Customizer.*
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.stereotype.Component
 
 @Configuration
 @EnableWebSecurity
+//@Component
+//https://docs.spring.io/spring-security/reference/servlet/configuration/kotlin.html
+//https://stackoverflow.com/questions/74206724/how-to-override-securityfilterchain-in-spring-boot-context
+//https://github.com/spring-projects/spring-boot/issues/33103#issuecomment-1310815121
 class SecurityConfig {
 
     @Bean
