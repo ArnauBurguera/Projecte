7e3e8dbadfa20abdf29736c89be79c23923728d2
Arnau Burguera Calles <arnau.burguera@glovoapp.com>
2024-10-01 12:51:56 +0200

    added package line and changed docker compose name

--- src/main/kotlin/arnau/projecte/config/SecurityConfig.kt
+++ src/main/kotlin/arnau/projecte/config/SecurityConfig.kt
@@ -1,3 +1,4 @@
+package arnau.projecte.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -5,14 +6,9 @@ import org.springframework.security.config.Customizer.*
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.web.SecurityFilterChain
-import org.springframework.stereotype.Component
 
 @Configuration
 @EnableWebSecurity
-//@Component
-//https://docs.spring.io/spring-security/reference/servlet/configuration/kotlin.html
-//https://stackoverflow.com/questions/74206724/how-to-override-securityfilterchain-in-spring-boot-context
-//https://github.com/spring-projects/spring-boot/issues/33103#issuecomment-1310815121
 class SecurityConfig {
 
     @Bean
