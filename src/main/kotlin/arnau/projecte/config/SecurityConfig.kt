9de39c4a5beeba9229e614c4e921c56fb1f0e966
Arnau Burguera Calles <arnau.burguera@glovoapp.com>
2024-10-01 12:56:49 +0200

    addded comments with next steps

--- src/main/kotlin/arnau/projecte/config/SecurityConfig.kt
+++ src/main/kotlin/arnau/projecte/config/SecurityConfig.kt
@@ -25,3 +25,11 @@ class SecurityConfig {
             .build()
     }
 }
+/*
+- Fer oauth amb google.
+- Intentar veure què fa internament spring security
+-Afegir docker-compose-local. On nomes tingui db per tal de que un cambio al jar i una nova build no requereixi de muntar tota la db again i només corro la app en intellij.
+
+-Un bash script(que es com una task en llenguatge maquina) per fer el build automaticament.
+-I un altre bash script per insertar 1mil usuaris a la db amb un init.sql o algo així, no cal que sigui migration de moment només crearlos amb un random UUID i
+   que faci cherrypicking d'una llita de 5 noms o així. La idea final es fer un que creei com 8M i aleshores provar a fer un redis per guaradr la base a la cache o així i veure la diferencia*/
