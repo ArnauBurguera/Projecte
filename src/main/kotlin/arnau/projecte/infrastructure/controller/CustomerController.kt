4459599f47d4dd39fcf549724578cdae49bd6ba4
Arnau Burguera Calles <arnau.burguera@glovoapp.com>
2024-10-11 15:29:37 +0200

    moved comments

--- src/main/kotlin/arnau/projecte/infrastructure/controller/CustomerController.kt
+++ src/main/kotlin/arnau/projecte/infrastructure/controller/CustomerController.kt
@@ -40,3 +40,15 @@ class CustomerController(
         return ResponseEntity.ok(page.content)
     }
 }
+/*
+- Intentar veure què fa internament spring security
+-Afegir docker-compose-local. On nomes tingui db per tal de que un cambio al jar i una nova build no requereixi de muntar tota la db again i només corro la app en intellij.
+
+-Un bash script(que es com una task en llenguatge maquina) per fer el build automaticament.
+-I un altre bash script per insertar 1mil usuaris a la db amb un init.sql o algo així, no cal que sigui migration de moment només crearlos amb un random UUID i
+   que faci cherrypicking d'una llita de 5 noms o així. La idea final es fer un que creei com 8M i aleshores provar a fer un redis per guaradr la base a la cache o així i veure la diferencia
+- https://www.youtube.com/watch?v=PczgM2L3w60 and https://www.youtube.com/watch?v=KYNR5js2cXE for more tasty security
+-add envars maybe to store
+-Take a look at openapi schema definitions and how can you apply them to here https://openapi-generator.tech/
+
+*/
