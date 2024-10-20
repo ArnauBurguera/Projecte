3a1d7cae01e1b717ccea6e2bbc5a7f0e15e02177
Arnau Burguera Calles <arnau.burguera@glovoapp.com>
2024-10-20 12:11:36 +0200

    Issue with loading envvars from applicproper

--- src/main/kotlin/arnau/projecte/ProjecteApplication.kt
+++ src/main/kotlin/arnau/projecte/ProjecteApplication.kt
@@ -3,7 +3,7 @@ package arnau.projecte
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
-@SpringBootApplication//(scanBasePackages = ["arnau.projecte.config"])
+@SpringBootApplication
 class ProjecteApplication
 
 fun main(args: Array<String>) {
