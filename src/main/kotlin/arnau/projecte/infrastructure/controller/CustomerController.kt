afc9d9c9b4fc8d0715b7a9cb1f63a6258c33cf4b
Arnau Burguera Calles <arnau.burguera@glovoapp.com>
2024-10-29 12:57:53 +0100

    added task comment

--- src/main/kotlin/arnau/projecte/infrastructure/controller/CustomerController.kt
+++ src/main/kotlin/arnau/projecte/infrastructure/controller/CustomerController.kt
@@ -43,6 +43,10 @@ class CustomerController(
 /*
 - Veure video del pavo de oath i implementar e q faci.
 - solucionar application.properties no llegint .env
+
+-sh run carrega les envvars minim dels minims (les envvars.sh)
+-sh reset local db (2: crear base dades i fer el insert/seed)
+
 -Un bash script(que es com una task en llenguatge maquina) per fer el build automaticament.
 -I un altre bash script per insertar 1mil usuaris a la db amb un init.sql o algo així, no cal que sigui migration de moment només crearlos amb un random UUID i
    que faci cherrypicking d'una llita de 5 noms o així. La idea final es fer un que creei com 8M i aleshores provar a fer un redis per guaradr la base a la cache o així i veure la diferencia
