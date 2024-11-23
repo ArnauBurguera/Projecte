5b3ab735bfb8aecf56435ab121e739a54de5713f
Arnau Burguera Calles <arnau.burguera@glovoapp.com>
2024-11-23 20:20:43 +0100

    Create RedisConfig.kt

---
+++ src/main/kotlin/arnau/projecte/config/RedisConfig.kt
@@ -0,0 +1,27 @@
+package arnau.projecte.config
+
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.redis.connection.RedisConnectionFactory
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
+import org.springframework.data.redis.core.RedisTemplate
+import org.springframework.data.redis.serializer.StringRedisSerializer
+
+@Configuration
+class RedisConfig {
+
+    @Bean
+    fun redisConnectionFactory(): RedisConnectionFactory {
+        return LettuceConnectionFactory("localhost", 6379)
+    }
+
+    @Bean
+    fun redisTemplate(): RedisTemplate<String, Any> {
+        val template = RedisTemplate<String, Any>()
+        template.connectionFactory = redisConnectionFactory()
+        template.keySerializer = StringRedisSerializer()
+        template.valueSerializer = StringRedisSerializer()
+        return template
+    }
+}
